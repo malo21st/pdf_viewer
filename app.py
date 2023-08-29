b0e7a8dcf1ba77ac3f31a50041d445c00c7aff4f
malo21st <44739330+malo21st@users.noreply.github.com>
2023-08-29 09:43:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_option('public_folder', 'downloads')
+st.set_option('public_folder', 'static')
 st.set_option('public_filetypes', 'pdf')
 st.markdown('<a href="./static/NOBDATA_ChatGPT活用個別サービス開発資料.pdf" target="_blank">PDFを表示</a>', unsafe_allow_html=True)
 st.markdown('<a href="/static/NOBDATA_ChatGPT活用個別サービス開発資料.pdf" target="_blank">PDFを表示</a>', unsafe_allow_html=True)
