f944a96d5ff7aff520a0b2edd93180c32138e675
malo21st <44739330+malo21st@users.noreply.github.com>
2023-08-29 12:02:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.config.set_option('public_folder', 'static')
 st.config.set_option('public_filetypes', 'pdf')
-st.markdown('<a href="./app/static/NOBDATA_ChatGPT活用個別サービス開発資料.pdf" target="_blank">PDFを表示</a>', unsafe_allow_html=True)
+st.markdown('<a href="https://drive.google.com/file/d/1p69mAARqhB7HratNZgZizPJhcc7aAXOt/view?usp=sharing" target="_blank">PDFを表示</a>', unsafe_allow_html=True)
 st.markdown('<a href="/app/static/NOBDATA_ChatGPT活用個別サービス開発資料.pdf" target="_blank">PDFを表示</a>', unsafe_allow_html=True)
 st.markdown(
     '<img src="./app/static/tenjikai.png" height="333" style="border: 5px solid orange">',
