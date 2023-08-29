e1f469cc06b01c5b948575d7f7ca9816766a3a7a
malo21st <44739330+malo21st@users.noreply.github.com>
2023-08-29 09:01:22 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown('<a href="./static/NOBDATA_ChatGPT活用個別サービス開発資料.pdf" target="_blank">PDFを表示</a>', unsafe_allow_html=True)
+st.markdown('<a href="/static/NOBDATA_ChatGPT活用個別サービス開発資料.pdf" target="_blank">PDFを表示</a>', unsafe_allow_html=True)
